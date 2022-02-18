379b3d26a7ccafc5c91d2798916226a3a3578ab6
calebdamian <naranjocaleb@gmail.com>
2022-02-17 21:41:52 -0500

    Added focus() function and debugged some visual bugs

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function compute()
 {
-   var principal = document.getElementById("principal").value;
-   var rate = document.getElementById("rate").value;
-   var years = document.getElementById("years").value;
-   var interest = principal * years * rate /100;
-   var year = new Date().getFullYear()+parseInt(years);
-   var result = document.getElementById("result");
-   result.innerHTML = "<strong>If you deposit <span>"+principal+"</span>,\<br\> at an interest rate of <span>"+rate+"</span>%.\<br\> You will receive an amount of <span>"+ interest+"</span>,\<br\>in the year <span>"+year+"</span>\<br\></strong>"
+    if(validarCampo()){
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate /100;
+        var year = new Date().getFullYear()+parseInt(years);
+        var result = document.getElementById("result");
+        result.innerHTML = "<strong>If you deposit <span>"+principal+"</span>,\<br\> at an interest rate of <span>"+rate+"</span>%.\<br\> You will receive an amount of <span>"+ interest+"</span>,\<br\>in the year <span>"+year+"</span>\<br\></strong>"
+    }else{
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+ 
 }
 
 function updateRate(){
@@ -17,8 +23,6 @@ function updateRate(){
 function validarCampo(){
     var principal = document.getElementById("principal").value;
     if(principal<0 || principal==0 || principal ==""){
-        alert("Enter a positive number");
-        principal.focus();
         return false;
     }else{
 
