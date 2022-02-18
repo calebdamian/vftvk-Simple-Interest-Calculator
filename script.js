2e6ddaa82da831c3ce9522f824656390a5c108c0
calebdamian <naranjocaleb@gmail.com>
2022-02-17 21:20:49 -0500

    Final hands-on-lab V1

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var years = document.getElementById("years").value;
+   var interest = principal * years * rate /100;
+   var year = new Date().getFullYear()+parseInt(years);
+   var result = document.getElementById("result");
+   result.innerHTML = "<strong>If you deposit <span>"+principal+"</span>,\<br\> at an interest rate of <span>"+rate+"</span>%.\<br\> You will receive an amount of <span>"+ interest+"</span>,\<br\>in the year <span>"+year+"</span>\<br\></strong>"
 }
-        
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval+"%";
+}
+
+function validarCampo(){
+    var principal = document.getElementById("principal").value;
+    if(principal<0 || principal==0 || principal ==""){
+        alert("Enter a positive number");
+        principal.focus();
+        return false;
+    }else{
+
+        return true;
+    }
+
+}
